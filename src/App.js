caa082bcf396adb6223b1c1c1aef0746197b1df8
Lzygmunt <lzygmunt0@gmail.com>
2022-02-13 18:06:29 +0100

    feat: create text breaking frame

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './styles.scss';
 
 const App = () => (
-  <div className="App">
-
-  </div>
+  <>
+    <div className="block" />
+    <div className="frame-cover">
+      <div className="text-block top-text">
+        <p className="text"><span>Lorem ipsum</span></p>
+      </div>
+      <div className="text-block bottom-text">
+        <p className="text">
+          <span>
+            Ea eum eveniet excepturi itaque
+            <br />
+            <br />
+            labore, nostrum suscipit tempore ut.
+          </span>
+        </p>
+      </div>
+    </div>
+  </>
 );
 
 export default App;
